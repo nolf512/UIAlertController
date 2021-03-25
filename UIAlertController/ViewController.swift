f6ae2edb4f8368b88f3d1264a41df5d6d61d625a
nofprog <k.jin3217@gmail.com>
2021-03-25 18:08:35 +0900

    UIAlertAction

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -13,7 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        let ac = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "タイトル1", style: .default, handler: nil))
+        ac.addAction(UIAlertAction(title: "タイトル2", style: .default, handler: nil))
+        ac.addAction(UIAlertAction(title: "タイトル3", style: .default, handler: nil))
+        ac.addAction(UIAlertAction(title: "タイトル4", style: .destructive, handler: nil))
+        ac.addAction(UIAlertAction(title: "タイトル5", style: .cancel, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
+    
 
 }
 
