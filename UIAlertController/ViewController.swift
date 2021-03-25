ece48be33cca87a1d545d05e661bdeebc36be07e
nofprog <k.jin3217@gmail.com>
2021-03-25 19:22:00 +0900

    addTextFieldを持つalert(button2)

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var yamaguchiMaho = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,14 +19,30 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonTapped(_ sender: Any) {
-        let ac = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .actionSheet)
+        let ac = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "タイトル1", style: .default, handler: nil))
         ac.addAction(UIAlertAction(title: "タイトル2", style: .default, handler: nil))
         ac.addAction(UIAlertAction(title: "タイトル3", style: .default, handler: nil))
         ac.addAction(UIAlertAction(title: "タイトル4", style: .destructive, handler: nil))
         ac.addAction(UIAlertAction(title: "タイトル5", style: .cancel, handler: nil))
+        
+//        let title1 = UIAlertAction(title: "タイトル１", style: .default, handler: nil)
+//        ac.addAction(title1)
+        
         present(ac, animated: true, completion: nil)
     }
+    
+    
+    @IBAction func button2Tapped(_ sender: Any) {
+        
+        let ac = UIAlertController(title: "テキストを入力", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        ac.addTextField()
+        present(ac, animated: true, completion: nil)
+    
+    
+    }
+    
     
 
 }
