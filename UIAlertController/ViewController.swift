0f9bc93cfe6f218850f175ece0477942fdd39a83
nofprog <k.jin3217@gmail.com>
2021-04-13 21:58:25 +0900

    UIAlertController変更

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 0
 
     @IBOutlet var countLabel: UILabel!
     
@@ -21,60 +20,13 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonTapped(_ sender: Any) {
-        let ac = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "タイトル1", style: .default, handler: nil))
-        ac.addAction(UIAlertAction(title: "タイトル2", style: .default, handler: nil))
-        ac.addAction(UIAlertAction(title: "タイトル3", style: .default, handler: nil))
-        ac.addAction(UIAlertAction(title: "タイトル4", style: .destructive, handler: nil))
-        ac.addAction(UIAlertAction(title: "タイトル5", style: .cancel, handler: nil))
-        
-//        let title1 = UIAlertAction(title: "タイトル１", style: .default, handler: nil)
-//        ac.addAction(title1)
-        
+        let ac = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "default", style: .default, handler: nil))
+        ac.addAction(UIAlertAction(title: "destructive", style: .destructive, handler: nil))
+        ac.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: nil))
         present(ac, animated: true, completion: nil)
     }
-    
-    
-    @IBAction func button2Tapped(_ sender: Any) {
         
-        let ac = UIAlertController(title: "テキストを入力", message: nil, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        ac.addTextField()
-        present(ac, animated: true, completion: nil)
-    
-    
-    }
-    
-    @IBAction func plus(_ sender: Any) {
-        
-        count += 1
-        countLabel.text = String(count)
-        
-        if count >= 21 {
-            let ac = UIAlertController(title: "20です", message: nil, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-
-            present(ac, animated: true, completion: nil)
-            
-            count = 0
-            countLabel.text = String(count)
-            
-        }
-        
-    }
-    
-    
-    
-    @IBAction func minus(_ sender: Any) {
-        
-        count -= 1
-        countLabel.text = String(count)
-        
-    }
-    
-    
-    
-    
 
 }
 
